34baba1cca1a85db50de8ee510b9d9faf3c519a3
Pavel Krikunov <fusrodah.df@mail.ru>
2023-03-30 22:43:53 +0300

    add searchDepartments

--- server/src/routers/departmentRouter.ts
+++ server/src/routers/departmentRouter.ts
@@ -1,14 +1,11 @@
 import {t} from '../trpc';
-import adminController from '../controllers/adminController';
+import departmentController from '../controllers/departmentController';
 import {z} from "zod";
-import EmployeeController from "../controllers/employeeController";
 
 const addDepartmentProcedure = t.procedure.input(z.object(
     {
         name: z.string(),
-        surname: z.string(),
-        post: z.string(),
-        departmentId: z.number()
+        description: z.string(),
     }))
 
 const deleteDepartmentProcedure = t.procedure.input(z.object(
@@ -16,22 +13,32 @@ const deleteDepartmentProcedure = t.procedure.input(z.object(
         Id:z.number()
     }))
 
+const searchDepartmentsProcedure = t.procedure.input(z.object(
+    {
+        name:z.string().optional()
+    }))
+
 
 
 export const departmentRouter = t.router({
     addEmloyee: addDepartmentProcedure
         .mutation(async (req) => {
-            const result = await EmployeeController.addOne(req.input.name, req.input.surname, req.input.post, req.input.departmentId,);
+            const result = await departmentController.addOne(req.input.name, req.input.description);
             return result;
         }),
     deleteEmployee: deleteDepartmentProcedure
         .mutation(async (req) => {
-            const result = await EmployeeController.deleteOne(req.input.Id);
+            const result = await departmentController.deleteOne(req.input.Id);
             return result;
         }),
-    getAllDepartment: t.procedure
-        .query(async () => {
-            const result = await EmployeeController.getAll();
+    getAllDepartments: t.procedure
+        .query(async (req) => {
+            const result = await departmentController.getAll();
+            return result;
+        }),
+    searchDepartments: searchDepartmentsProcedure
+        .query(async (req) => {
+            const result = await departmentController.search(req.input.name);
             return result;
         }),
 
