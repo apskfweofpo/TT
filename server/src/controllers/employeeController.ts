cdedbfc05cca5026739fdc46900030f328233e59
Pavel Krikunov <fusrodah.df@mail.ru>
2023-03-30 21:42:59 +0300

    create employeeController.ts

---
+++ server/src/controllers/employeeController.ts
@@ -0,0 +1,13 @@
+class EmployeeController{
+    async addOne(name, surname, post, departmentId){
+
+    }
+    async deleteOne(Id){
+
+    }
+    async getAll(){
+
+    }
+}
+
+export default new EmployeeController()
