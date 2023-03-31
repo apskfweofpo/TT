3c15cad7023f07a4b3efab8f47c8c75dac1e7c15
Pavel Krikunov <fusrodah.df@mail.ru>
2023-03-31 22:06:12 +0300

    change port and add function for start server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,17 @@
-import express from "express"
-import cors from "cors"
+import express, {Express} from "express";
+import cors from "cors";
 import {createExpressMiddleware} from "@trpc/server/adapters/express";
-import {appRouter} from "./routers/index"
+import {appRouter} from "./routers/index";
 
-const PORT = 3000
-const app = express()
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(cors({origin: process.env.CORS_ORIGINS}));
+app.use("/api", createExpressMiddleware({router: appRouter}));
 
+const start = async () => {
+    app.listen(PORT, () => console.log(`Server start: ${PORT}`));
+};
 
-app.use(cors({origin: "http://localhost:5173"}))
-app.use("/api", createExpressMiddleware({router: appRouter}))
-app.listen(PORT, () => console.log(`Server start: ${PORT}`))
+start();
 
-
-export type AppRouter = typeof appRouter
+export type AppRouter = typeof appRouter;
